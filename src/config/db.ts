8760e4419e61f943e45061d006ef1e0365e85f61
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2023-09-25 16:14:31 +0100

    fix: updated database url connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST } = process.env;
-
-const sequelize = new Sequelize(
-	`postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:5432/${DATABASE_NAME}`,
-	{ logging: false }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, { logging: false });
 
 export default sequelize;
